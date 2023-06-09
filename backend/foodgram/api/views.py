bc12788b03194544e0f671bc5193cf03c6e918cf
Алмаз Миннибаев <almaz200@bk.ru>
2023-06-09 21:16:58 +0300

    nginx

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -5,7 +5,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 
 from recipes.models import (Favorite, Ingredient, IngredientAmount, Recipe,
