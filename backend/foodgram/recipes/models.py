20dead01456489cce85c7a2273e68e745a3ae444
Алмаз Миннибаев <almaz200@bk.ru>
2023-06-09 21:51:08 +0300

    nginx

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator, RegexValidator
 from django.db import models
 
+from api.validators import validate_time
 
 User = get_user_model()
 
